a7a50cf7b4c69ad005efa62093eeba922a06ec86
zsghhw <zsghhw@icloud.com>
2024-06-10 22:23:43 +0100

    Initial Commit

---
+++ Swift3D/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift3D
+//
+//  Created by 张训 on 2024/6/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift3D/Swift3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift3DApp.swift
+//  Swift3D
+//
+//  Created by 张训 on 2024/6/10.
+//
+
+import SwiftUI
+
+@main
+struct Swift3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
